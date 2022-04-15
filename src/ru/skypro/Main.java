63007de2614e1fa8f31de8dc18232f76d1099fb0
Alexey <lexshadrin@mail.ru>
2022-04-15 12:36:09 +1100

    Дополнительные задачи. На проверку.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,90 +4,131 @@ public class Main {
 
     public static void main(String[] args) {
 	// Задача №1
-        byte a = 3;
-        short b = 7;
-        int age = 11;
-        long d = 17L;
-        float e = 2.5f;
-        double f = 7.3;
-        char g = 64;
-        System.out.println("@ = " + g);
-        boolean i = age >= 18;
-        System.out.println(i);
-
+        {
+            byte a = 3;
+            short b = 7;
+            int age = 11;
+            long d = 17L;
+            float e = 2.5f;
+            double f = 7.3;
+            char g = 64;
+            System.out.println("@ = " + g);
+            boolean i = age >= 18;
+            System.out.println(i);
+        }
     // Задача №2
-        float boxer1 = 78.2f;
-        float boxer2 = 82.7f;
-        float totalWeight = boxer1 + boxer2;
-        float weightDifference = boxer2 - boxer1;
-        System.out.println("Общий вес боксеров: " + totalWeight + " кг.");
-        System.out.println("Разница в весе боксеров: " + weightDifference + " кг.");
+        {
+            float boxer1 = 78.2f;
+            float boxer2 = 82.7f;
+            float totalWeight = boxer1 + boxer2;
+            float weightDifference = boxer2 - boxer1;
+            System.out.println("Общий вес боксеров: " + totalWeight + " кг.");
+            System.out.println("Разница в весе боксеров: " + weightDifference + " кг.");
+        }
 
     // Задача №3
-        short banana = 5;
-        short bananaWeight = 80; // вес ингредиентов в граммах
-        short milk = 200; // милилитры
-        short milkWeight = 105; // вес 100 мл. в граммах
-        short milkWeightMilliliters = 100;
-        short iceCream = 2;
-        short iceCreamWeight = 100;
-        short eggs =  4;
-        short eggsWeight = 70;
-        int bananaTotal = banana * bananaWeight;
-        int milkTotal = milk * milkWeight / milkWeightMilliliters;
-        int iceCreamTotal = iceCream * iceCreamWeight;
-        int eggsTotal = eggs * eggsWeight;
-        float sportBreakfast = bananaTotal + milkTotal + iceCreamTotal + eggsTotal;
-        short gram = 1000;
-        float sportBreakfastKg = sportBreakfast / gram;
-        System.out.println("Вес спортивного завтрака " + sportBreakfastKg + " кг.");
+        {
+            short banana = 5;
+            short bananaWeight = 80; // вес ингредиентов в граммах
+            short milk = 200; // милилитры
+            short milkWeight = 105; // вес 100 мл. в граммах
+            short milkWeightMilliliters = 100;
+            short iceCream = 2;
+            short iceCreamWeight = 100;
+            short eggs = 4;
+            short eggsWeight = 70;
+            int bananaTotal = banana * bananaWeight;
+            int milkTotal = milk * milkWeight / milkWeightMilliliters;
+            int iceCreamTotal = iceCream * iceCreamWeight;
+            int eggsTotal = eggs * eggsWeight;
+            float sportBreakfast = bananaTotal + milkTotal + iceCreamTotal + eggsTotal;
+            short gram = 1000;
+            float sportBreakfastKg = sportBreakfast / gram;
+            System.out.println("Вес спортивного завтрака " + sportBreakfastKg + " кг.");
+        }
 
     // Задача №4
-        byte excessWeight = 7;
-        short kg = 1000;
-        short weightLoos1 = 250;
-        short weightLoos2 = 500;
-        int excessWeightKilogram = excessWeight * kg;
-        int totalDay250g = excessWeightKilogram / weightLoos1;
-        int totalDay500g = excessWeightKilogram / weightLoos2;
-        int totalDayAverage = (totalDay250g + totalDay500g) / 2;
+        {
+            byte excessWeight = 7;
+            short kg = 1000;
+            short weightLoos1 = 250;
+            short weightLoos2 = 500;
+            int excessWeightKilogram = excessWeight * kg;
+            int totalDay250g = excessWeightKilogram / weightLoos1;
+            int totalDay500g = excessWeightKilogram / weightLoos2;
+            int totalDayAverage = (totalDay250g + totalDay500g) / 2;
 
-        System.out.println("Худея по 250 грамм в день потребуется: " + totalDay250g + " дней");
-        System.out.println("Худея по 500 грамм в день потребуется: " + totalDay500g + " дней");
-        System.out.println("В среднем для похудения потребуется: " + totalDayAverage + " день");
+            System.out.println("Худея по 250 грамм в день потребуется: " + totalDay250g + " дней");
+            System.out.println("Худея по 500 грамм в день потребуется: " + totalDay500g + " дней");
+            System.out.println("В среднем для похудения потребуется: " + totalDayAverage + " день");
+        }
 
     // Задача №5
-        float incomeMonthMasha = 67_760f;
-        float incomeMonthDenis = 83_690f;
-        float incomeMonthKristina = 76_230f;
-        byte percent100 = 100;
-        byte percent10 = 10;
-        byte month = 12;
+        {
+            float incomeMonthMasha = 67_760f;
+            float incomeMonthDenis = 83_690f;
+            float incomeMonthKristina = 76_230f;
+            byte percent100 = 100;
+            byte percent10 = 10;
+            byte month = 12;
             // Годовой доход сотрудников до повышения зарплаты
-        float incomeYearMasha = incomeMonthMasha * month;
-        float incomeYearDenis = incomeMonthDenis * month;
-        float incomeYearKristina = incomeMonthKristina * month;
+            float incomeYearMasha = incomeMonthMasha * month;
+            float incomeYearDenis = incomeMonthDenis * month;
+            float incomeYearKristina = incomeMonthKristina * month;
             // Месячная зарплата после повышения на 10%
-        float incomeNewMonthMasha = ((incomeMonthMasha / percent100) * percent10) + incomeMonthMasha;
-        float incomeNewMonthDenis = ((incomeMonthDenis / percent100) * percent10) + incomeMonthDenis;
-        float incomeNewMonthKristina = ((incomeMonthKristina / percent100) * percent10) + incomeMonthKristina;
+            float incomeNewMonthMasha = ((incomeMonthMasha / percent100) * percent10) + incomeMonthMasha;
+            float incomeNewMonthDenis = ((incomeMonthDenis / percent100) * percent10) + incomeMonthDenis;
+            float incomeNewMonthKristina = ((incomeMonthKristina / percent100) * percent10) + incomeMonthKristina;
             // Годовой доход после повышения зарплаты
-        float incomeNewYearMasha = incomeNewMonthMasha * month;
-        float incomeNewYearDenis = incomeNewMonthDenis * month;
-        float incomeNewYearKristina = incomeNewMonthKristina * month;
+            float incomeNewYearMasha = incomeNewMonthMasha * month;
+            float incomeNewYearDenis = incomeNewMonthDenis * month;
+            float incomeNewYearKristina = incomeNewMonthKristina * month;
             // Разница в годовом доходе до и после повышения
-        float differentYearMasha = incomeNewYearMasha - incomeYearMasha;
-        float differentYearDenis = incomeNewYearDenis - incomeYearDenis;
-        float differentYearKristina = incomeNewYearKristina - incomeYearKristina;
+            float differentYearMasha = incomeNewYearMasha - incomeYearMasha;
+            float differentYearDenis = incomeNewYearDenis - incomeYearDenis;
+            float differentYearKristina = incomeNewYearKristina - incomeYearKristina;
 
-        System.out.println("Месячный доход Маши стал: " + incomeNewMonthMasha +
-                            " p. доход вырос на: " + differentYearMasha + "р.");
+            System.out.println("Месячный доход Маши стал: " + incomeNewMonthMasha +
+                    " p. доход вырос на: " + differentYearMasha + "р.");
 
-        System.out.println("Месячный доход Дениса стал: " + incomeNewMonthDenis +
-                           " p. доход вырос на: " + differentYearDenis + "р.");
+            System.out.println("Месячный доход Дениса стал: " + incomeNewMonthDenis +
+                    " p. доход вырос на: " + differentYearDenis + "р.");
 
-        System.out.println("Месячный доход Кристины стал: " + incomeNewMonthKristina +
-                            " p. доход вырос на: " + differentYearKristina + "р.");
+            System.out.println("Месячный доход Кристины стал: " + incomeNewMonthKristina +
+                    " p. доход вырос на: " + differentYearKristina + "р.");
+        }
 
+    // Задача №6
+        {
+            int a = 12;
+            int b = 27;
+            int c = 44;
+            int d = 15;
+            int e = 9;
+            int result = a * (b + (c - d * e));
+            result = 0 - result; // Либо сразу записать: int result = 0 - (a * (b + (c - d * e)));
+            System.out.println("Результат = " + result);
+        }
+
+        // Задача №7
+        {
+            int a = 7;
+            int b = 5;
+            b = (a + b) - (a = (a + b - a));
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+
+        // Задача №8
+        {
+            int a = 256;
+            int b = 0;
+//            b = ((a - ((a % 100) % 10)) % 100) / 10;
+            b = a % 100;
+            b = b % 10;
+            b = (a - b) % 100;
+            b = b / 10;
+            System.out.println("Центральное значение переменной а = " + b);
+        }
     }
 }
